7972450a1fec44539737a0235be4a29a1997a8a9
Hassam009 <dev.hassam@irsolutions.tech>
2026-04-28 16:11:45 +0500

    added validation pipe

--- src/auth/dto/signup.dto.ts
+++ src/auth/dto/signup.dto.ts
@@ -1,12 +1,14 @@
-import { IsEmail, IsString, MinLength } from 'class-validator';
+import { IsEmail, IsString, MinLength, IsNotEmpty } from 'class-validator';
 
 export class SignupDto {
   @IsString()
+  @IsNotEmpty()
   name: string;
 
   @IsEmail()
   email: string;
 
+  @IsString()
   @MinLength(6)
   password: string;
 }
